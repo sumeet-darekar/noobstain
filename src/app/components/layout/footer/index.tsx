e75e6a85646046e3d2d866fbabe9370d71efc98e
sumeet <79735377+sumeet-darekar@users.noreply.github.com>
2025-01-24 08:40:15 +0530

    Update index.tsx

--- src/app/components/layout/footer/index.tsx
+++ src/app/components/layout/footer/index.tsx
@@ -9,13 +9,6 @@ export const Footer = () => {
       <nav>
         <div className="grid grid-flow-col gap-4">
           <ul className="menu menu-horizontal gap-1">{renderSocials()}</ul>
-          <iframe
-src="https://aichatbot.sendbird.com/playground/index.html?app_id=DE440E09-5C5C-49A7-A842-C1A90344D5C3&bot_id=5Tn9EKJZJIHVl4Oz4P_Ya&region=ap-5"
-width="100%"
-style="height: 100%; min-height: 700px"
-frameborder="0"
-></iframe>
-
         </div>
       </nav>
     </footer>
